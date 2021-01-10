faf61439ecce1d17e1201f112025dd274949488f
atBeatty <adam.t.beatt@gmail.com>
2021-01-09 20:21:23 -0800

    Done.

---
+++ src/components/Bands.js
@@ -0,0 +1,22 @@
+
+import React, { Component } from 'react'
+import Band from './Band'
+
+
+const Bands = (props) => {
+    const renderBands = props.bands.map(band => <Band key={band.id} band={band} deleteBand={props.deleteBand}/>)
+
+
+    return (
+        <div>
+            {renderBands}
+        </div>
+    )
+
+
+}
+
+
+
+
+export default Bands;
